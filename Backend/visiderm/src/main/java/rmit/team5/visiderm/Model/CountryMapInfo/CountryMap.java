b010e1a8c9a6185e89e7484196670a544c5482d6
54754N4 <35555527+54754N4@users.noreply.github.com>
2019-12-16 10:52:29 +0700

    Created CountryMap, Hospital, UserAccount & UserRole entities

---
+++ Backend/visiderm/src/main/java/rmit/team5/visiderm/Model/CountryMapInfo/CountryMap.java
@@ -0,0 +1,51 @@
+package rmit.team5.visiderm.Model.CountryMapInfo;
+
+import org.hibernate.annotations.Cascade;
+import rmit.team5.visiderm.Model.HospitalInfo.Hospital;
+
+import javax.persistence.*;
+import java.util.List;
+
+import static org.hibernate.annotations.CascadeType.MERGE;
+import static org.hibernate.annotations.CascadeType.SAVE_UPDATE;
+
+@Entity
+@Table(name = "map")
+public class CountryMap {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "mapID")
+    private Long id;
+
+    @Lob
+    @Column(name="mapImage", nullable=false, columnDefinition="mediumblob")
+    private byte[] map;
+
+    @OneToMany(mappedBy = "map", fetch = FetchType.LAZY, orphanRemoval = true)
+    @Cascade({SAVE_UPDATE, MERGE})
+    private List<Hospital> hospitalList;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public byte[] getMap() {
+        return map;
+    }
+
+    public void setMap(byte[] map) {
+        this.map = map;
+    }
+
+    public List<Hospital> getHospitalList() {
+        return hospitalList;
+    }
+
+    public void setHospitalList(List<Hospital> hospitalList) {
+        this.hospitalList = hospitalList;
+    }
+}
---
+++ Backend/visiderm/src/main/java/rmit/team5/visiderm/Model/UserInfo/UserRole.java
@@ -0,0 +1,48 @@
+package rmit.team5.visiderm.Model.UserInfo;
+
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+
+import static org.hibernate.annotations.CascadeType.SAVE_UPDATE;
+
+@Entity
+@Table(name = "role")
+public class UserRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "roleID")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name= "accountID")
+    @Cascade(SAVE_UPDATE)
+    private UserAccount account;
+
+    public UserAccount getAccount() {
+        return account;
+    }
+
+    public void setAccount(UserAccount account) {
+        this.account = account;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
