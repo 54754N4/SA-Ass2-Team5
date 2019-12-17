5af2366f6ac53fb8876c3de4550d533fab6c089b
54754N4 <35555527+54754N4@users.noreply.github.com>
2019-12-17 22:07:58 +0700

    Added lesion DAO repo queries

--- Backend/visiderm-external/src/main/java/rmit/team5/external/DAO/Interface/ILesionDAO.java
+++ Backend/visiderm-external/src/main/java/rmit/team5/external/DAO/Interface/ILesionDAO.java
@@ -1,8 +1,17 @@
 package rmit.team5.external.DAO.Interface;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import rmit.team5.external.Model.LesionInfo.Lesion;
 
+@Repository
 public interface ILesionDAO extends JpaRepository<Lesion, Long> {
+    @Query("select lesion from Lesion lesion where lesion.visitID like ?1")
+    Page<Lesion> getLesionsOfVisit(String visitID, Pageable pageable);
 
+    @Query("select lesion from Lesion lesion")
+    Page<Lesion> getLesions(Pageable pageable);
 }
