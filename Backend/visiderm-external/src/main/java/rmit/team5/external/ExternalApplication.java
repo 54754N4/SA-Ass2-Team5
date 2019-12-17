385ef929ccb656d0ad2d19cf80cd64e1cc6c888f
54754N4 <35555527+54754N4@users.noreply.github.com>
2019-12-17 16:59:30 +0700

    Created new external application

---
+++ Backend/visiderm-external/src/main/java/rmit/team5/external/ExternalApplication.java
@@ -0,0 +1,11 @@
+package rmit.team5.external;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalApplication.class, args);
+    }
+}
