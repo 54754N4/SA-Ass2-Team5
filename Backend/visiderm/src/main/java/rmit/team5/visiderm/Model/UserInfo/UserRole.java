75d7589aaf8523fbbdeba57f142dc4d8957e3f58
54754N4 <35555527+54754N4@users.noreply.github.com>
2019-12-17 14:38:28 +0700

    Made user roles unique

--- Backend/visiderm/src/main/java/rmit/team5/visiderm/Model/UserInfo/UserRole.java
+++ Backend/visiderm/src/main/java/rmit/team5/visiderm/Model/UserInfo/UserRole.java
@@ -15,7 +15,7 @@ public class UserRole {
     @Column(name = "roleID")
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     @ManyToOne
