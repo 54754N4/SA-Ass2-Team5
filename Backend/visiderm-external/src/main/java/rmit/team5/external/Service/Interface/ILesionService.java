689ce05a10e7ccda69cf0ef97f752995c587a252
54754N4 <35555527+54754N4@users.noreply.github.com>
2019-12-18 10:48:41 +0700

    Added method to retrieve a visits lesions

--- Backend/visiderm-external/src/main/java/rmit/team5/external/Service/Interface/ILesionService.java
+++ Backend/visiderm-external/src/main/java/rmit/team5/external/Service/Interface/ILesionService.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 public interface ILesionService {
     HashMap<String, Object> getLesionsBySize(int page, boolean desc);
     HashMap<String, Object> getLesionsByDate(int page, boolean desc);
+    HashMap<String, Object> getLesionsOfVisit(int page);
     Lesion getLesion(long lesionID);
 
     boolean add(LesionDTO lesionDTO);
