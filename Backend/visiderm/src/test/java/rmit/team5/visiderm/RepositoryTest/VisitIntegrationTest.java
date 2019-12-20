202861c4941a3ad85a30c3eeabbf44f8217f2a96
54754N4 <35555527+54754N4@users.noreply.github.com>
2019-12-20 18:39:37 +0700

    Final postman collection

--- Backend/visiderm/src/test/java/rmit/team5/visiderm/RepositoryTest/VisitIntegrationTest.java
+++ Backend/visiderm/src/test/java/rmit/team5/visiderm/RepositoryTest/VisitIntegrationTest.java
@@ -2,6 +2,8 @@ package rmit.team5.visiderm.RepositoryTest;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,4 +11,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class VisitIntegrationTest {
+
+
+    private static Pageable getPageable() {
+        return PageRequest.of(0, 100);
+    }
 }
